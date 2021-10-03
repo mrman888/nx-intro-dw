47d48bce87a6342b8fa9c67d63c0380e0ab86bcc
mrman888 <amiibospam@gmail.com>
2021-10-03 15:32:45 +0200

    added api

---
+++ dw/apps/api/src/app/app.service.ts
@@ -0,0 +1,24 @@
+import { Injectable } from '@nestjs/common';
+
+interface Todo {
+  title: string;
+}
+
+@Injectable()
+export class AppService {
+  todos: Todo[] = [{ title: 'Todo 1' }, { title: 'Todo 2' }];
+
+  getWelcomeData(): string {
+    return 'Welcome to api!';
+  }
+
+  getData(): Todo[] {
+    return this.todos;
+  }
+
+  addTodo() {
+    this.todos.push({
+      title: `New todo ${Math.floor(Math.random() * 1000)}`,
+    });
+  }
+}
