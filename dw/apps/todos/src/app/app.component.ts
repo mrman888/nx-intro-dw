d32ac965f95f3ddf50bbb21bb15357c0f2d6129b
mrman888 <amiibospam@gmail.com>
2021-10-03 15:12:01 +0200

    update

--- dw/apps/todos/src/app/app.component.ts
+++ dw/apps/todos/src/app/app.component.ts
@@ -1,10 +1,21 @@
 import { Component } from '@angular/core';
 
+interface Todo {
+  title: string;
+}
+
 @Component({
   selector: 'dw-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
-  title = 'todos';
+  todos: Todo[] = [{ title: 'Todo 1' }, { title: 'Todo 2' }];
+
+  addTodo() {
+    // uncomment to fix the e2e test
+    //   this.todos.push({
+    //     title: `New todo ${Math.floor(Math.random() * 1000)}`,
+    //   });
+  }
 }
